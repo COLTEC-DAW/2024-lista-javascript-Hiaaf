26ba39001ce6bcf23c116a92239c783bbc1dce01
Hiago <hiaaf_@hotmail.com>
2024-06-12 11:13:55 -0300

    Tabuleiro de Xadrez

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function Triangulo() {
-  let numLinhas = parseInt(prompt("Número de linhas para o triângulo?"));
+  const numLinhas = parseInt(prompt("Número de linhas para o triângulo?"));
 
   let tri = "";
   for (let i = 0; i < numLinhas; i++) {
@@ -9,4 +9,20 @@ function Triangulo() {
     tri += "\n";
   }
   console.log(tri);
+}
+
+function Xadrez() {
+  const NumLinhas = parseInt(prompt("Número de linhas para o xadrez?"));
+  const NumColunas = parseInt(prompt("Número de colunas para o xadrez?"));
+
+  let xad = "";
+  for (let l = 0; l < NumLinhas; l++) {
+    if (l % 2 == 1)
+      xad += " ";
+    for (let c = 0; c < NumColunas; c++) {
+      xad += "# ";
+    }
+    xad += "\n";
+  }
+  console.log(xad);
 }
