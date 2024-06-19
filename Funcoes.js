ecdb7a5cde3aeb86cd94371fda1dfa53e55398ec
Hiago <hiaaf_@hotmail.com>
2024-06-19 08:34:39 -0300

    Contando caracteres

--- Funcoes.js
+++ Funcoes.js
@@ -10,14 +10,14 @@ function minMax() {
     return a > b ? a : a < b ? b : null;
   }
 
-  let minOrMax = prompt("Quer usar a função min ou a max?").toLowerCase();
+  const minOrMax = prompt("Quer usar a função min ou a max?").toLowerCase();
   if (!"minmax".includes(minOrMax)) {
     alert('Escreva "min" ou "max"');
     return;
   }
 
-  let a = parseInt(prompt("Passe o primeiro número"));
-  let b = parseInt(prompt("Passe o segundo número"));
+  const a = parseInt(prompt("Passe o primeiro número"));
+  const b = parseInt(prompt("Passe o segundo número"));
 
   if (minOrMax === "min") {
     let result = min(a, b);
@@ -50,9 +50,26 @@ function recursividade() {
     return mod(num - _mod, _mod);
   }
 
-  let num1 = parseInt(prompt("Informe um número positivo."));
-  let num2 = parseInt(prompt("Informe um segundo número positivo."));
+  const num1 = parseInt(prompt("Informe um número positivo."));
+  const num2 = parseInt(prompt("Informe um segundo número positivo."));
 
   console.log(`${num1} ${mod2(num1) ? "é" : "não é"} divisível por 2.`);
   console.log(`${num1} ${mod(num1, num2) ? "é" : "não é"} divisível por ${num2}.`);
+}
+
+// Contando caracteres
+function contandoChars() {
+  function countChars(frase, c) {
+    let count = 0;
+    for (let i = 0; i < frase.length; i++) {
+      if (frase[i] === c)
+        count++;
+    }
+    return count;
+  }
+
+  const frase = prompt("Informe a frase.");
+  const c = prompt("Informe o caracter a ser contado.");
+  
+  console.log(`'${c}' aparece ${countChars(frase, c)} vezes em "${frase}"`);
 }
